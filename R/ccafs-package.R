330f911dd95e3d4052ffd2dacb992534abcfcde4
Scott Chamberlain <myrmecocystus@gmail.com>
2017-01-04 16:24:32 -0800

    fix #14 add bit about how to get to pkg vignette

--- R/ccafs-package.R
+++ R/ccafs-package.R
@@ -1,4 +1,8 @@
-#' Client for CCAFS GCM Data
+#' @title Client for CCAFS GCM Data
+#'
+#' @description Client for Climate Change, Agriculture, and Food Security
+#' (CCAFS) General Circulation Models (GCM) data. Data is stored in
+#' Amazon S3, from which we provide functions to fetch data.
 #'
 #' @importFrom rappdirs user_cache_dir
 #' @importFrom httr GET stop_for_status write_disk progress content
@@ -8,4 +12,8 @@
 #' @docType package
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @keywords package
+#'
+#' @section Vignette:
+#' You can access the vignette like \code{vignette(package = "ccafs")} or
+#' \code{vignette("ccafs_vignette", package = "ccafs")}
 NULL
