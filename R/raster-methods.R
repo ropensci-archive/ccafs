da0002b83837fa64ce0cc62af2327a908ae8e94a
Scott Chamberlain <myrmecocystus@gmail.com>
2017-01-06 10:49:09 -0800

    re-export raster::plot to namespace, fix #12

---
+++ R/raster-methods.R
@@ -0,0 +1,12 @@
+#' raster::plot
+#'
+#' re-exported \code{plot} method from \code{raster} package to avoid failing
+#' when trying to plot raster objects without having \code{raster} loaded
+#' first.
+#'
+#' @name plot
+#' @rdname plot
+#' @keywords internal
+#' @export
+#' @importFrom raster plot
+NULL
